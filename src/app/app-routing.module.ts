677aa7721bb248a30a401df21ac7844a85b3a9f2
Malo <malomthethwa@outlook.com>
2020-04-28 17:10:07 +0200

    Router Added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ArticleListComponent } from './article-list/article-list.component';
+import { AboutComponent } from './about/about.component';
 
-const routes: Routes = [];
+
+//Nav Bar Routing
+const routes: Routes = [
+
+ {path: 'articles', component: ArticleListComponent},
+ {path: 'about', component: AboutComponent},
+ {path: '', component: ArticleListComponent},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
